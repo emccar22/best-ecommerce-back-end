2788b3e6eeb2691b697938d24133c7f8e1b5ae8e
emccar22 <e.mccarthy2285@gmail.com>
2022-01-29 12:02:51 -0500

    add models aand associations

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,34 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
+const { Product, Tag } = require('.');
+
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
